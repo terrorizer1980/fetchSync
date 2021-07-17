c4af0cae45f434250dd7d2b0617395b5001371df
Matheus sampaio Queiroga <srherobrine20@gmail.com>
2021-07-17 20:52:27 -0300

    Push

--- index.js
+++ index.js
@@ -6,47 +6,37 @@ const fs = require("fs");
 
 function fetchSync(url = "https://google.com", options = {
     Binary: false,
+    binary: false,
     mode: "cors"
 }){
-    try {
-        var ExecResult = child_process.execFileSync("node", [path.resolve(__dirname, "./lib/fetchSync.js")], {
-            env: {
-                ...process.env,
-                urlRequest: url,
-                FetchOptions: JSON.stringify(options)
-            }
-        }).toString("utf8");
-        
-        // Bin Function
-        if (options.Binary) {
-            ExecResult = ExecResult.split("TmpFetch*****");
-            ExecResult = ExecResult[ExecResult.length - 1];
-            ExecResult = ExecResult.split("\n").filter(a=>a);
-            ExecResult = path.resolve(ExecResult)
-            const FsBuffer = fs.readFileSync(ExecResult);
-            fs.rmSync(ExecResult, {force: true})
-            return {
-                Buffer: FsBuffer,
-                save: function(savePath = "./tmpFile.tmp") {
-                    savePath = path.resolve(savePath)
-                    fs.writeFileSync(savePath, FsBuffer, "binary");
-                    return savePath
-                }
-            }
-        } else {
-            return {
-                text: function(){return ExecResult},
-                json: function(){return JSON.parse(ExecResult)},
+    var ExecResult = child_process.execFileSync("node", [path.resolve(__dirname, "./lib/fetchSync.js")], {
+        env: {
+            ...process.env,
+            urlRequest: url,
+            FetchOptions: JSON.stringify(options)
+        }
+    }).toString("utf8");
+    
+
+    // Bin Function
+    if ((options.Binary || options.binary)) {
+        ExecResult = path.resolve(ExecResult.split("\n").filter(a=>a).join(""))
+        const FsBuffer = fs.readFileSync(ExecResult);
+        fs.rmSync(ExecResult, {force: true})
+        const Func = {
+            Buffer: FsBuffer,
+            save: function(savePath = "./tmpFile.tmp") {
+                savePath = path.resolve(savePath)
+                fs.writeFileSync(savePath, FsBuffer, "binary");
+                return savePath
             }
         }
-    } catch(err) {
-        const code = err.status;
-        const stdout = err.stdout.toString();
-        const stderr = err.stderr.toString();
-        if (code === 14 || code === 15) {
-            throw {stdout, stderr}
-        } else {
-            throw new Error("uncontrolled code", stdout, stderr)
+        Func["SavePath"] = Func.save
+        return Func
+    } else {
+        return {
+            text: function(){return ExecResult},
+            json: function(){return JSON.parse(ExecResult)},
         }
     }
 }
--- test.js
+++ test.js
@@ -6,7 +6,7 @@ const { resolve } = require("path");
 // Home Page Google
 try {
     const GoogleHomePage = fetchSync("https://google.com");
-    console.log(GoogleHomePage.text());
+    GoogleHomePage.text()
 } catch (error) {
     console.log("Unable to fetch the Google page");
     exit(1);
@@ -15,7 +15,7 @@ try {
 // Get Bds Maneger Server JSON
 try {
     const BdsManegerServers = fetchSync("https://raw.githubusercontent.com/The-Bds-Maneger/Raw_files/main/Server.json");
-    console.log(BdsManegerServers.json());
+    BdsManegerServers.json()
 } catch (error) {
     console.log("Unable to get JSON from Server");
     exit(2);
@@ -25,7 +25,7 @@ try {
 try {
     const php_urls = fetchSync("https://raw.githubusercontent.com/The-Bds-Maneger/Php_Static_Binary/main/binarys.json").json();
     const urlPhp = php_urls["linux"]["x64"];
-    console.log(urlPhp);
+    // console.log(urlPhp);
     const php_zip = fetchSync(urlPhp, {
         Binary: true
     });
